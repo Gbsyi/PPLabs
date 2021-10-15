02e65fe40d7cdf1e5d8991cbc7bf1925e26e44c5
Gbsyi <gbsyi@yandex.ru>
2021-10-15 19:23:35 +0700

    .

---
+++ Lab3/Lab5/Match.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Match
+    {
+        DateTime Date;
+        string Location;
+        (int FirstTeam, int SecondTeam) result;
+        Team FirstTeam;
+        Team SecondTeam;
+        List<Player> Players;
+        
+        public Match(DateTime date, string location, Team firstTeam, Team secondTeam)
+        {
+            Date = date;
+            Location = location;
+            FirstTeam = firstTeam;
+            SecondTeam = secondTeam;
+        }
+        
+        public void setScore(int firstTeamResult, int secondTeamResult)
+        {
+            result.FirstTeam = firstTeamResult;
+            result.SecondTeam = secondTeamResult;
+        }
+    }
+}
