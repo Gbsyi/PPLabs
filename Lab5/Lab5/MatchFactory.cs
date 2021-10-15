6575315bfbb0ec4f874b4a39637e37770e27a4e6
Gbsyi <gbsyi@yandex.ru>
2021-10-15 20:58:25 +0700

    Lab 5

---
+++ Lab5/Lab5/MatchFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class MatchFactory
+    {
+        public IMatch Create(string location, DateTime date, Team first, Team second)
+        {
+            Match newMatch = new(location, date, first, second);
+            foreach(Player player in first.Players)
+            {
+                newMatch.Players.Add(player);
+            }
+            foreach(Player player in second.Players)
+            {
+                newMatch.Players.Add(player);
+            }
+            return newMatch;
+        }
+    }
+}
