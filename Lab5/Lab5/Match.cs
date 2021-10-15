bfe17db887b1d98a8fbc0f6287d7dd5a776f1682
Gbsyi <gbsyi@yandex.ru>
2021-10-15 21:10:52 +0700

    Revert "Revert "Lab 5""
    This reverts commit bac13c9fa18559cd71c7185c5061e10d852af47a.

---
+++ Lab5/Lab5/Match.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Match : IMatch
+    {
+        private static int _counter = 1;
+        public int Id;
+        public DateTime Date;
+        public string Location;
+        public (int First, int Second) Result;
+        public Team FirstTeam;
+        public Team SecondTeam;
+        public List<Player> Players;
+        public Match(string location, DateTime date, Team first, Team second)
+        {
+            Id = _counter++;
+            Location = location;
+            Date = date;
+            FirstTeam = first;
+            SecondTeam = second;
+            Players = new();
+        }
+    }
+}
---
+++ Lab5/Lab5/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Player : IPlayer
+    {
+        private static int _counter = 1;
+        public int Id;
+        public string Name;
+        public Team PlayerTeam;
+        public Player(string name)
+        {
+            Id = _counter++;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
